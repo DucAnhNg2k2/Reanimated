b4b0b7dabe5350668e6c500173c11bfc311968d0
DucAnh <ngdanh2k2@gmail.com>
2023-07-19 00:36:20 +0700

    animation

--- App.tsx
+++ App.tsx
@@ -5,17 +5,95 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet} from 'react-native';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
-import Page1 from './src/Pages/Page1';
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
 
-function App(): JSX.Element {
+const SIZE = 75;
+const App = () => {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(1);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const gesTureHandler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart(event, context) {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive(event, context) {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd(event) {
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+    },
+  });
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [
+        {translateX: translateX.value},
+        {translateY: translateY.value},
+        {scale: scale.value},
+        {rotate: `${progress.value * 2 * Math.PI}rad`},
+      ],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(0.5, {duration: 1000}),
+      Infinity,
+      true,
+    );
+    scale.value = withRepeat(withTiming(1.5, {duration: 1000}), Infinity, true);
+  }, []);
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Page1 />
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.container}>
+      <PanGestureHandler onGestureEvent={gesTureHandler}>
+        <Animated.View style={[styles.view, reanimatedStyle]} />
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  view: {
+    height: SIZE,
+    width: SIZE,
+    backgroundColor: 'blue',
+  },
+});
 
 export default App;
